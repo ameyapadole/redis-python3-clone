0203e8929af068ed7a8f55548c5a7b0475c16475
Ameya <ameyapadole26@gmail.com>
2024-05-12 13:55:01 -0700

    second stage commit

--- app/main.py
+++ app/main.py
@@ -8,13 +8,14 @@ import socket
 
 def main():
     print("Logs from your program will appear here!")
+    pong = "+PONG\r\n"
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     connection, address = server_socket.accept()
     with connection:
         connection.recv(1024) #1024 number of bytes we can recieve
         connection.send(pong.encode()) #Send accepts a utf string to be send
 
-    pong = "+PONG\r\n"
+
 
 
 if __name__ == "__main__":
